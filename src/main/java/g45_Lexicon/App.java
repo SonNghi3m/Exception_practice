130c11220b7cf94b713f89f638afa397e6ec6781
Son Nghiem <nghiemminhson@gmail.com>
2022-12-05 14:22:22 +0100

    Initial commit

---
+++ src/main/java/g45_Lexicon/App.java
@@ -0,0 +1,13 @@
+package g45_Lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
